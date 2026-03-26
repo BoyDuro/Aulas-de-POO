166e7998eb495cfe167026ea59a72d264c9e7295
BoyDuro <mmsantiago07@gmail.com>
2026-03-26 17:26:32 +0000

    terminei

--- Aula4/ex2.py
+++ Aula4/ex2.py
@@ -1,2 +1,21 @@
 print('Digite dois valores inteiros separados por um operador +, -, * ou /')
 valores = input('Escreva: ')
+
+for b in valores:
+    if b == '+' or b == '-' or b == '*' or b == '/':
+        calc = b
+pos = valores.find(calc)
+numero1 = int(valores[:pos])
+numero2 = int(valores[pos:])
+
+match calc:
+    case '+':
+        resultado = numero1 + numero2
+    case '-':
+        resultado = numero1 - numero2
+    case '*':
+        resultado = numero1 * numero2
+    case '/':
+        resultado = numero1 / numero2
+
+print(f'O resultado da operação é {resultado}')
