c235bf341dc0e10882f79537a0443747cfffe146
BoyDuro <mmsantiago07@gmail.com>
2026-04-28 18:23:05 +0000

    Lista 3

---
+++ Lista 3/q1.py
@@ -0,0 +1,56 @@
+class Viagem:
+    def __init__(self, v, d, l):
+        self.set_Destino(v)
+        self.set_Distancia(d)
+        self.set_litros(l)
+    def set_Destino(self, v):
+        if len(v) > 0:
+            self.__v = v
+        else:
+            raise ValueError()
+    def set_Distancia(self, d):
+        if d > 0:
+            self.__d = d
+        else:
+            raise ValueError()
+    def set_litros(self, l):
+        if l > 0:
+            self.__l = l
+        else:
+            raise ValueError()
+    def get_destino(self):
+        return self.__v
+    def get_distancia(self):
+        return self.__d
+    def get_litros(self):
+        return self.__l
+    def consumo(self):
+        return self.__d / self.__l
+    def __str__(self):
+        return f'A viagem para {self.get_destino()}, teve distância = {self.get_distancia()} e gasto em litros {self.get_litros()}'
+
+
+class UI:
+    def main():
+        op = 0
+        while op != 2:
+            op = UI.menu()
+            if op == 1:
+                UI.calculo()
+
+    @staticmethod
+    def menu():
+        print('1 - Calcular   2 - Fim')
+        op = int(input('Digite sua escolha: '))
+        return op
+
+    @staticmethod
+    def calculo():
+        v = input('Digite seu destino: ')
+        d = float(input('Digite a distância percorrida em Km: '))
+        l = float(input('Digite o gasto de combustível em litros: '))
+        x = Viagem(v, d, l)
+        print(x)
+        print(f'O gasto total da viagem foi = {x.consumo()}')
+
+UI.main()
---
+++ Lista 3/q2.py
@@ -0,0 +1,55 @@
+class Pais:
+    def __init__(self, n, p, a):
+        self.set_nome(n)
+        self.set_populacao(p)
+        self.set_area(a)
+    def set_nome(self, n):
+        if len(n) > 0:
+            self.__n = n
+        else:
+            raise ValueError()
+    def set_populacao(self, p):
+        if p > 0:
+            self.__p = p
+        else:
+            raise ValueError()
+    def set_area(self, a):
+        if a > 0:
+            self.__a = a
+        else:
+            raise ValueError()
+    def get_nome(self):
+        return self.__n
+    def get_populacao(self):
+        return self.__p
+    def get_area(self):
+        return self.__a
+    def Densidade(self):
+        return self.__p / self.__a
+    def __str__(self):
+        return f'O {self.get_nome()} tem população = {self.get_populacao()} e área {self.get_area()}'
+
+class UI:
+    def main():
+        op = 0
+        while op != 2:
+            op = UI.menu()
+            if op == 1:
+                UI.calculo()
+
+    @staticmethod
+    def menu():
+        print('1 - Calcular   2 - Fim')
+        op = int(input('Digite sua escolha: '))
+        return op
+    
+    @staticmethod
+    def calculo():
+        n = input('Digite o nome do país: ')
+        p = float(input('Digite a população do país: '))
+        a = float(input('Digite a área do país: '))
+        x = Pais(n, p, a)
+        print(x)
+        print(f'Dito isso, têm densidade demográfica = {x.Densidade()}')
+
+UI.main()
